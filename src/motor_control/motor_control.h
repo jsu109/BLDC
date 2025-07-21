c919787111a48620050eaef492eb8bc4ea349d61
Joel Suddaby <joel.suddaby@taitcommunications.com>
2025-07-21 16:40:31 +1200

    started creating some software structure and planning

---
+++ src/motor_control/motor_control.h
@@ -0,0 +1,14 @@
+#ifndef MOTOR_CONTROL_H
+#define MOTOR_CONTROL_H
+
+// planning
+/*This module is a high level module which is hardware independent. will talk only 
+to motor_hal.h not directly to GPIO or SPI or PWM. 
+This module will achieve the following functionality:
+- Initalise Control setup for motor driver eg -call PWM setup routines
+- Commutation Logic. 
+- speed control. 
+- fault handling.
+*/
+
+#endif
---
+++ src/motor_control/motor_hal.h
@@ -0,0 +1,6 @@
+#ifndef MOTOR_HAL_H
+#define MOTOR_HAL_H
+// Planning
+/*This is where things like specific hardware is called to implement motor control
+*/
+#endif //MOTOR_HAL_H
