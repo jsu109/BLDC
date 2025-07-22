3692c1f079c593853e47c5f54cfc775b4e09e246
Joel Suddaby <joel.suddaby@taitcommunications.com>
2025-07-22 13:44:04 +1200

    some changes to rp2040 spi implementation

--- lib/rp2040/rp2040_spi.c
+++ lib/rp2040/rp2040_spi.c
@@ -1,12 +1,12 @@
-
 #include "rp2040_spi.h"
-
+#include "hardware/spi.h"
 void rp2040_spi_init(void) {
     // RP2040-specific SPI init
 }
 
 void rp2040_spi_setFormat(const SPI_settings_t *settings) {
     // Set baudrate, polarity, phase etc for RP2040 SPI
+    static void spi_set_format (spi_inst_t *spi, uint data_bits, spi_cpol_t cpol, spi_cpha_t cpha, __unused spi_order_t order)
 }
 
 uint16_t rp2040_spi_transfer16(uint16_t data) {
