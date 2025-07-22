d9a052283538673bd772b92ea2a5b20026a8df15
Joel Suddaby <joel.suddaby@taitcommunications.com>
2025-07-22 14:57:47 +1200

    spi hal init implementation for AS5048A encoder

--- src/encoder/AS5048A.c
+++ src/encoder/AS5048A.c
@@ -1,6 +1,6 @@
 #include "stdint.h"
 #include "AS5048A.h"
-#include "hardware/spi.h"
+#include "spi_hal.h"
 
 typedef struct {
     
@@ -11,7 +11,18 @@ static AS5048A_t AS5048A;
  void AS5048AInit(void)
 {
     // Initialization for AS5048A
-    
+    SPI_settings_t AS5048A_spiSettings = {
+    .sysType = RP2040,
+    .baudrate = 80000,
+    .dataBits = AS5048A_DATA_BITS,
+    .order = AS5048A_BITORDER,
+    .cpol = 0,
+    .cpha = 0,
+    .hw_handle = spi0
+    };
+    SPI_hal_t AS5048A_spiInst;
+    spi_hal_init(&AS5048A_spiInst, &AS5048A_spiSettings);
+    spi_hal_config(&AS5048A_spiInst,&AS5048A_spiSettings);
 }
 
  void AS5048ASetConfigReg(void) 
