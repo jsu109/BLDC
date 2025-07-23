be677117c396313c259b5c3b44a508ac40b15ad3
Joel Suddaby <joel.suddaby@taitcommunications.com>
2025-07-23 12:40:03 +1200

    removed dependency of hardware/gpio in main, now using gpio_hal. need to test

--- src/main.c
+++ src/main.c
@@ -2,7 +2,8 @@
 #include "pico/stdlib.h"
 #include "hardware/timer.h"
 #include "encoder_hal.h"
-int led =1;
+#include "gpio_hal.h"
+
 int64_t alarm_callback(alarm_id_t id, void *user_data) {
     // Put your timeout handler code in here
     return 0;
@@ -10,19 +11,23 @@ int64_t alarm_callback(alarm_id_t id, void *user_data) {
 
 
 encoderHal_t encoder1;
-
+GPIO_hal_t led;
+GPIO_settings_t led_settings = {
+    .gpioPin = 25,
+    .out = 1,
+    .sysType = RP2040
+};
 
 int main()
 {
     stdio_init_all();
-    gpio_init(25);
-    gpio_set_dir(25, GPIO_OUT);
-    gpio_put(25,1);
+    gpio_hal_init(&led,&led_settings);
+    gpio_hal_put(&led,&led_settings,1);
     encoder1.id = encoder_ID_AS5048A;
     bool res = encoderHalInit(encoder1.id, &encoder1);
     // Timer example code - This example fires off the callback after 2000ms
     // add_alarm_in_ms(2000, alarm_callback, NULL, false);
-    gpio_put(25,0);
+    gpio_hal_put(&led,&led_settings,res);
     while (true) {
         uint16_t angle = encoder1.read();
         float angle_degrees = encoder1.process(angle);
