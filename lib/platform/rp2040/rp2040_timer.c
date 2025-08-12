31c3388462db969141501798e84e8d3f443c1070
jsu109 <suddaby.joel@gmail.com>
2025-08-12 23:39:10 +1200

    changes made

--- lib/platform/rp2040/rp2040_timer.c
+++ lib/platform/rp2040/rp2040_timer.c
@@ -1,14 +1,13 @@
 #include "rp2040_timer.h"
 #include "hardware/timer.h"
-#include <stdio.h>
 void rp2040_timer_init(timer_hal_t *timerInst)
 {
 
 }
-uint32_t rp2040_timer_get_ms()
+uint32_t rp2040_timer_get_ms(timer_hal_t *timerInst)
 {
     uint64_t us = time_us_64();
-    return (uint32_t)(us / 1000);
+    return (uint16_t)(us / 1000);
 
 }
 void rp2040_timer_delay_ms(timer_hal_t *timerInst, uint32_t ms)
